020a324fe56887f67f7ec7607a226ea63bd0e4d7
denisp <denis.parkhomchuk@gmail.com>
2024-09-10 22:36:43 +0300

    calculator

---
+++ tests/test_calculator.py
@@ -0,0 +1,12 @@
+from src.calculator import Calculator
+import pytest
+
+
+@pytest.mark.parametrize(
+    "x, y, res",
+    [(1, 2, 0.5),
+     (5, -1, -5),
+     ]
+)
+def test_divide(x, y, res):
+    assert Calculator().divide(x, y) == res
