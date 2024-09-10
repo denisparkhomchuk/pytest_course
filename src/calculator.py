9dde54ca05deb54a7a0f4012ee0cb60a07df9961
denisp <denis.parkhomchuk@gmail.com>
2024-09-10 23:06:06 +0300

    calculator

--- src/calculator.py
+++ src/calculator.py
@@ -4,6 +4,8 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both args should be numeric")
         return x/y
 
     def add(self, x: Union[int, float], y: Union[int, float]) -> int | float:
